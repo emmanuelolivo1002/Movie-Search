64434d3df0ac61b1b8cad2623792614d23d9117f
Emmanuel Olivo <emmanuelo1002@gmail.com>
2018-05-11 14:11:05 -0500

    Add search and get request

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-  res.send("Welcome");
+  res.render("search");
 });
 
 app.get("/results", function(req, res) {
-  request("http://www.omdbapi.com/?apikey=thewdb&s=baby", function (error, response, body) {
+  var query = req.query.search;
+  var url = "http://www.omdbapi.com/?apikey=thewdb&s=" + query;
+  request(url, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var data = JSON.parse(body);
       res.render("results", {data: data});
